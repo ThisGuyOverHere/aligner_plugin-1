4215a11e1f363b43d0e329aab20fb055ecf4cb9e
Marco Ciotoli <marco@kromin.it>
2018-06-25 13:09:43 +0200

    Init class

--- lib/Features/Aligner.php
+++ lib/Features/Aligner.php
@@ -1,7 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vincenzoruffa
- * Date: 25/06/18
- * Time: 12:43
- */
+    /**
+     * Created by PhpStorm.
+     * User: vincenzoruffa
+     * Date: 25/06/18
+     * Time: 12:43
+     */
+
+
+    namespace Features;
+
+    class Aligner extends BaseFeature
+    {
+
+        const FEATURE_CODE = "aligner";
+
+
+    }
