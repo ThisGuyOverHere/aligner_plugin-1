35f09d011629ed73eb40c3d959dffda90bfc2554
Marco Ciotoli <marco@kromin.it>
2018-07-02 12:50:53 +0200

    Init project component

--- app/src/Components/Project/Project.js
+++ app/src/Components/Project/Project.js
@@ -1,6 +1,22 @@
 import React, {Component} from 'react';
 
 class ProjectComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            project: {
+                config: {
+                    password: this.props.match.params.password,
+                    id: this.props.match.params.projectID
+                }
+            }
+        };
+    }
+    static getDerivedStateFromProps(props, state){
+        //console.log(props,state);
+        return this.state
+    }
     render() {
         return (
             <div>
