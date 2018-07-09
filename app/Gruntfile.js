8b25add002e8904cc16a3a73a4101d1bf7095f42
Marco Ciotoli <marco@kromin.it>
2018-07-09 11:54:28 +0200

    Improvement code base

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
     const es2015Preset = require('babel-preset-es2015');
     const reactPreset = require('babel-preset-react');
+    const babelstage2 = require('babel-preset-stage-2');
     const sass = require('node-sass');
 
     /*var es2015Preset = require('babel-preset-es2015');*/
@@ -24,7 +25,7 @@ module.exports = function(grunt) {
             components: {
                 options: {
                     transform: [
-                        [ 'babelify', { presets: [ es2015Preset, reactPreset ] } ]
+                        [ 'babelify', { presets: [ es2015Preset, reactPreset,babelstage2 ] } ]
                     ],
                     browserifyOptions: {
                         paths: [ __dirname + '/node_modules' ]
--- app/src/Components/Home/Home.component.js
+++ app/src/Components/Home/Home.component.js
@@ -2,10 +2,19 @@ import React, {Component} from 'react';
 import PropTypes from "prop-types";
 
 class HomeComponent extends Component {
+    constructor() {
+        super();
+    }
+
+    bar = () => {
+        console.log(this.props.label);
+    };
+
     render() {
         return (
             <div>
                 <p>{this.props.label}</p>
+                <button onClick={this.bar}>prova</button>
             </div>
         );
     }
