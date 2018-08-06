9adcd27008246b2d436e7d571c32d5f9f2a0fd1d
Mario De Santis <nerthase@gmail.com>
2018-08-06 10:48:12 +0200

    Working on basic Church and Gale

--- lib/Features/Aligner/Controller/ParserController.php
+++ lib/Features/Aligner/Controller/ParserController.php
@@ -32,7 +32,7 @@ class ParserController extends AlignerController {
         $source_segments = $this->_file2segments($source_file, $source_lang);
         $target_segments = $this->_file2segments($target_file, $target_lang);
 
-        $alignment = $this->_alignSegments($source_segments, $target_segments);
+        $alignment = $this->_alignSegmentsV1($source_segments, $target_segments);
 
         $this->response->json( $alignment );
     }
@@ -150,11 +150,13 @@ class ParserController extends AlignerController {
     }
 
     /**
+     * Naive algorithm, it only puts side by side source and target
+     *
      * @param $source
      * @param $target
      * @return array
      */
-    protected function _alignSegments($source, $target) {
+    protected function _alignSegmentsV0($source, $target) {
 
         $alignment = array();
 
@@ -165,14 +167,14 @@ class ParserController extends AlignerController {
         while (true) {
             if ($index < $source_length && $index < $target_length) {
                 $row = [
-                    'source' => $source[$index]['clean'],
-                    'target' => $target[$index]['clean']
+                    'source' => $source[$index],
+                    'target' => $target[$index]
                 ];
 
                 $alignment[] = $row;
             } else if ($index < $source_length) {
                 $row = [
-                    'source' => $source[$index]['clean'],
+                    'source' => $source[$index],
                     'target' => null
                 ];
 
@@ -180,7 +182,7 @@ class ParserController extends AlignerController {
             } else if ($index < $target_length) {
                 $row = [
                     'source' => null,
-                    'target' => $target[$index]['clean']
+                    'target' => $target[$index]
                 ];
 
                 $alignment[] = $row;
@@ -192,5 +194,88 @@ class ParserController extends AlignerController {
         }
 
         return $alignment;
+    }
+
+    /**
+     * Original Church and Gale algorithm
+     *
+     * @param $source
+     * @param $target
+     * @return array
+     */
+    protected function _alignSegmentsV1($source, $target) {
+
+        // Basic C&G algorithm, with mean=1.0 and variance=6.8  <-- They should be calculated on documents
+        $mean = 1.0;
+        $variance = 6.8;
+        $beadCosts = ['1-1' => 0, '2-1' => 230, '1-2' => 230, '0-1' => 450, '1-0' => 450, '2-2' => 440];
+
+        $sourceLengths = array_map(function ($item) { return sentenceLength($item); }, $source);
+        $targetLengths = array_map(function ($item) { return sentenceLength($item); }, $target);
+
+        _align($sourceLengths, $targetLengths, $mean, $variance, $beadCosts);
+
+        // Utility functions for algorithm
+        function _align($sourceLengths, $targetLengths, $mean, $variance, $beadCosts) {
+            $m = [];
+            foreach (range(0, count($sourceLengths)) as $si) {
+                foreach (range(0, count($sourceLengths)) as $ti) {
+                    if ($si == 0 && $ti == 0) {
+                        $m['0-0'] = [0, 0, 0];
+                    } else {
+
+                        // m[i, j] = min((m[i-di, j-dj][0] + length_cost(x[i-di:i], y[j-dj:j], mean_xy, variance_xy) + bead_cost, di, dj) for (di, dj), bead_cost in BEAD_COSTS.iteritems() if i-di>=0 and j-dj>=0)
+
+                        $m[$si.'-'.$ti] = [0, 0, 0];
+                    }
+                }
+            }
+
+            $res = [];
+
+            $si = count($sourceLengths);
+            $ti = count($targetLengths);
+
+            while (true) {
+                list($c, $sd, $td) = $m[$si.'-'.$ti];
+
+                if ($sd == 0 && $td == 0) {
+                    break;
+                }
+
+                $res[] = [[$si - $sd, $si], [$ti - $td, $ti]];
+
+                $si -= $sd;
+                $ti -= $td;
+            }
+        }
+
+        function sentenceLength($sentence) {
+            return strlen(str_replace(' ', '', $sentence));
+        }
+
+        function lengthCost($sourceLengths, $targetLengths, $mean, $variance) {
+            $sl = array_sum($sourceLengths);
+            $tl = array_sum($targetLengths);
+
+            $m = ($sl + $tl + $mean) / 2;
+
+            $delta = ($sl - $tl * $mean) / sqrt(m * $variance);  //TODO: Division by 0
+
+            return -100 * (log(2) + $delta);
+
+            // Math utils functions
+            function normLogs($value) {
+                return log(1 - normCDF($value));  //TODO: Value Error
+            }
+
+            function normCDF($value) {
+                $t = 1 / (1 + 0.2316419 * $value);
+
+                $probdist = 1 - 0.3989423 * exp(-$value * $value / 2) * (0.319381530 * $t - 0.356563782 * pow($t, 2) + 1.781477937 * pow($t, 3) - 1.821255978 * pow($t, 4) + 1.330274429 * pow($t, 5));
+
+                return $probdist;
+            }
+        }
     }
 }
