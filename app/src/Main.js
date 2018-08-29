19ecb235aae6aa968eb50aeabe09a6bc03868a80
Marco Ciotoli <marco@kromin.it>
2018-08-29 11:32:39 +0200

    [FIX] Change name from project to job

--- app/src/Main.js
+++ app/src/Main.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {HashRouter} from 'react-router-dom'
 
 import HomeComponent from "./Components/Home/Home.component";
-import ProjectComponent from "./Components/Project/Project.component";
+import JobComponent from "./Components/Project/Job.component";
 import NotFoundComponent from "./Components/Shared/NotFound/NotFound.component"
 import {Switch} from "react-router";
 import Layout from "./Components/Shared/Layout/Layout.component";
@@ -18,7 +18,7 @@ class App extends Component {
                 <div className="App">
                     <Switch>
                         <Layout exact path="/" component={HomeComponent}/>
-                        <Layout path="/project/:jobID/:password" component={ProjectComponent}/>
+                        <Layout path="/project/:jobID/:password" component={JobComponent}/>
                         <Layout component={NotFoundComponent}/>
                     </Switch>
                 </div>
