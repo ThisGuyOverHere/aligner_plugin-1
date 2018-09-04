a58f67d21ba096116038c1bbd588a276b50d1495
Francesco Monti <francesco.monti@kromin.it>
2018-09-04 15:11:52 +0200

    Minor style fix, and code refactoring in SystemStore.

--- app/src/Stores/System.store.js
+++ app/src/Stores/System.store.js
@@ -11,10 +11,6 @@ EventEmitter.prototype.setMaxListeners(0);
 
 let SystemStore = assign({}, EventEmitter.prototype, {
 
-    statusLogin: false,
-    statusExportModal: false,
-    statusResetPasswordModal: false,
-
     emitChange: function (event, args) {
         this.emit.apply(this, arguments);
     },
@@ -25,15 +21,12 @@ let SystemStore = assign({}, EventEmitter.prototype, {
 AppDispatcher.register(function (action) {
     switch (action.actionType) {
         case SystemConstants.OPEN_LOGIN:
-            SystemStore.statusLogin = action.status;
             SystemStore.emitChange(SystemConstants.OPEN_LOGIN,action.status);
             break;
         case SystemConstants.OPEN_EXPORT_MODAL:
-            SystemStore.statusExportModal = action.status;
             SystemStore.emitChange(SystemConstants.OPEN_EXPORT_MODAL,action.status);
             break;
         case SystemConstants.OPEN_RESET_PASSWORD_MODAL:
-            SystemStore.statusResetPasswordModal = action.status;
             SystemStore.emitChange(SystemConstants.OPEN_RESET_PASSWORD_MODAL,action.status);
             break;
     }
