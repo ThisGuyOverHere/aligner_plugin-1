e79c078b5e344b186cf357fc4f9bea9769ae5544
Vincenzo Ruffa <vincenzoruffa1991@gmail.com>
2018-10-03 17:48:32 +0200

    improved merge method, got rid of some useless code lines

--- lib/Features/Aligner/Model/Segments/SegmentDao.php
+++ lib/Features/Aligner/Model/Segments/SegmentDao.php
@@ -233,10 +233,11 @@ class Segments_SegmentDao extends DataAccess_AbstractDao {
         $merge_count = 0;
 
         $segments_id = [];
-        foreach ($segments as $segment) {
+        foreach ($segments as $key => $segment) {
             $raw_merge   .= " " . $segment[ 'content_raw' ];
             $clean_merge .= " " . $segment[ 'content_clean' ];
             $merge_count += $segment[ 'raw_word_count' ];
+
             $segments_id[] = $segment['id'];
         }
 
@@ -245,6 +246,16 @@ class Segments_SegmentDao extends DataAccess_AbstractDao {
         self::updateSegmentContent(array_shift($segments_id),  array($raw_merge, $clean_merge, $hash_merge, $merge_count));
 
         self::deleteSegmentsByIds($segments_id);
+
+        $first_segment = array_shift($segments);
+
+        $first_segment[ 'content_raw' ]    = $raw_merge;
+        $first_segment[ 'content_clean' ]  = $clean_merge;
+        $first_segment[ 'raw_word_count' ] = $merge_count;
+        $first_segment[ 'order' ]          = (int)$first_segment[ 'order' ];
+        $first_segment[ 'next' ]           = (int)$first_segment[ 'next' ];
+
+        return $first_segment;
 
     }
 
