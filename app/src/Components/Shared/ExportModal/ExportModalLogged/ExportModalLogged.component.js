cab6ad558c90c8542a654530e39c44841e423828
Marco Ciotoli <marco@kromin.it>
2018-10-22 11:13:14 +0200

    Implement tmx list, select key and create new tmx

--- app/src/Components/Shared/ExportModal/ExportModalLogged/ExportModalLogged.component.js
+++ app/src/Components/Shared/ExportModal/ExportModalLogged/ExportModalLogged.component.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {getUserInitials} from "../../../../Helpers/SystemUtils.helper";
 import PropTypes from "prop-types";
+import {httpGetTmxList, httpCreateTmx, httpSaveTmx} from "../../../../HttpRequests/Tmx.http";
 
 class ExportModalLogged extends Component {
 
@@ -11,10 +12,34 @@ class ExportModalLogged extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cloudCheckBox: true
-
+            cloudCheckBox: true,
+            tmxList: [],
+            selected: null,
+            newTmx: null,
         };
     }
+
+    componentDidMount = () => {
+        this.fetchTmx();
+    };
+
+    fetchTmx = async () => {
+        try {
+            const res = await fetch('/plugins/aligner/tm/mine');
+            const list = await res.json();
+            this.setState({
+                tmxList: list,
+                selected: list[0]
+            });
+        } catch (e) {
+            console.error(e)
+        }
+        return true;
+    };
+
+    componentWillUnmount = () => {
+
+    };
 
     render() {
         return (
@@ -31,49 +56,109 @@ class ExportModalLogged extends Component {
                     </div>
                 </div>
 
-                <div className="memories">
-
-                    {[0,1,2].map( (element,index) => {
-                        return <div className="memory" key={index}>
-                            <div className="radio-container">
-                                <input type="radio" className="hidden" name="memory" value="name" tabIndex="2"></input>
-                            </div>
-                            <div className="memory-info">
-                                <p>MyPersonalTMX {index}</p>
-                                <p>1WWMSOMXHS213242</p>
-                            </div>
+                {this.state.tmxList.length > 0 ?
+                    <div>
+                        <div className="memories">
+                            {this.renderMemories()}
                         </div>
-                    })}
-                </div>
-
-                <div className="line"></div>
-
-                <div className="memory-input-container">
-                    <div className="radio-container">
-                        <input type="radio" className="hidden"name="memory" value="name" tabIndex="3"></input>
+                        <div className="line"></div>
                     </div>
-                    <div className="memory-input">
-                        <input type="text" tabIndex="4" placeholder="Create new resource"></input>
+                    : null}
+
+                {this.state.newTmx ?
+                    <div className="new-memory">
+                        <form onSubmit={this.saveMemory}>
+                            <input type="text" tabIndex="4" onChange={this.handleInput} placeholder="Description of tmx"/>
+                            <button type="submit" className="ui button">Save</button>
+                        </form>
+                        <p>{this.state.newTmx.key}</p>
                     </div>
-                </div>
+                    : <button className="ui button" onClick={this.createMemory}>Create new resource</button>}
+
 
                 <div className="selection">
                     <div className="ui checked toggle checkbox">
                         <input
                             type="checkbox" className="" name="cloud"
-                            readOnly=""  tabIndex="5" value={this.state.cloudCheckBox}
+                            readOnly="" tabIndex="5" value={this.state.cloudCheckBox}
                             onChange={this.cloudHandler}
                         ></input>
-                        <label className={ this.state.sendToCloud ? 'active' : 'inactive'} >Help to improve the public cloud</label>
+                        <label className={this.state.sendToCloud ? 'active' : 'inactive'}>Help to improve the public
+                            cloud</label>
                     </div>
                 </div>
 
                 <button className="export-btn ui button" tabIndex="6" type="">
-                     Export
+                    Export
                 </button>
             </div>
         );
     }
+
+    renderMemories = () => {
+        let memories = [];
+        this.state.tmxList.map((element, index) => {
+            const memory = <div className="memory" key={index}>
+                <div className="radio-container">
+                    <input type="radio" className="hidden" name="memory"
+                           checked={element.key === this.state.selected.key}
+                           onChange={this.handleCheckRadio}
+                           value={index} tabIndex={index}/>
+                </div>
+                <div className="memory-info">
+                    <p>{element.name? element.name : 'Private TM and Glossary'}</p>
+                    <p>{element.key}</p>
+                </div>
+            </div>;
+            memories.push(memory);
+            return element;
+        });
+
+        return memories;
+    };
+    handleCheckRadio = (e) =>{
+        this.setState({
+           selected: this.state.tmxList[e.target.value]
+        });
+    };
+    createMemory = () => {
+        httpCreateTmx().then((response) => {
+            const tmx = {
+                id: response.data.id,
+                key: response.data.key,
+                pass: response.data.pass,
+                name: null
+            };
+            this.setState({
+                newTmx: tmx
+            });
+        }, (error) => {
+
+        })
+    };
+    saveMemory = (e) => {
+        e.preventDefault();
+        httpSaveTmx(this.state.newTmx.key,this.state.newTmx.name).then((response)=>{
+            if(response.data){
+                let list = this.state.tmxList;
+                list.unshift(this.state.newTmx);
+                this.setState({
+                    tmxList: list,
+                    newTmx: null
+                });
+            }
+        }, (error)=>{
+           console.error(error)
+        });
+
+    };
+    handleInput = (e) =>{
+        let newTmx = this.state.newTmx;
+        newTmx.name = e.target.value;
+        this.setState({
+            newTmx: newTmx
+        });
+    };
 
     cloudHandler = () => {
         this.setState({
