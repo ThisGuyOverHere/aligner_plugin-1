9de1438e8d0f6c3824559a60aaf9435c45ba69a7
Francesco Monti <francesco.monti@kromin.it>
2018-11-12 11:37:19 +0100

    Solved warning generated by login action in Export Model.

--- app/src/Components/Shared/Login/Login.component.js
+++ app/src/Components/Shared/Login/Login.component.js
@@ -8,7 +8,8 @@ import {httpLogin, httpMe} from "../../../HttpRequests/System.http";
 class LoginComponent extends Component {
 
     static propTypes = {
-        googleLink: PropTypes.string
+        googleLink: PropTypes.string,
+        showLogin: PropTypes.func
     };
 
     constructor(props) {
@@ -154,10 +155,10 @@ class LoginComponent extends Component {
                 .then(() => {
                     httpMe().then(response => {
                         SystemActions.loggedIn(response.data.user, true);
-                        this.setState({
-                            error: false,
-                            logged: false,
-                        })
+                    });
+                    this.setState({
+                        error: false,
+                        logged: false,
                     })
                 })
                 .catch(error => {
