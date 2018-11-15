18b614130b95717cea2565b755115c2e4ce5ee63
Vincenzo Ruffa <vincenzoruffa1991@gmail.com>
2018-11-15 15:00:05 +0100

    error 404 where job is not found on getsegments

--- lib/Features/Aligner/Controller/SegmentsController.php
+++ lib/Features/Aligner/Controller/SegmentsController.php
@@ -8,7 +8,7 @@
 
 namespace Features\Aligner\Controller;
 
-use Exceptions\ValidationError;
+use Exceptions\NotFoundError;
 use Features\Aligner;
 use Features\Aligner\Model\Segments_SegmentDao;
 use Features\Aligner\Model\Jobs_JobDao;
@@ -22,28 +22,15 @@ class SegmentsController extends AlignerController {
 
         $job = Jobs_JobDao::getByIdAndPassword( $id_job, $password );
         if(empty($job)){
-            throw new ValidationError( "Invalid job id/password pair" );
+            throw new NotFoundError( "Invalid job id/password pair" );
         }
-        /*$where  = $this->request->param( 'where' );
-        $order_source  = $this->request->param( 'order_source' );
-        $order_target  = $this->request->param( 'order_target' );
-        $amount = $this->request->param( 'amount' );
 
-        $config = Aligner::getConfig();
-
-        if(empty($where)){$where = 'after';}
-        if(empty($order_source)){$order_source = 0;}
-        if(empty($order_target)){$order_target = 0;}
-        if(empty($amount)){$amount = $config['SEGMENT_AMOUNT_PER_PAGE'];}*/
-
-        //$target = Segments_SegmentDao::getTargetOrdered($id_job, $where, $order_target, $amount);
         $target = Segments_SegmentDao::getTypeOrderedByJobId($id_job, 'target');
         foreach ($target as $key => $segment){
             $target[$key]->content_raw   = Aligner\Utils\AlignUtils::_mark_xliff_tags($segment->content_raw);
             $target[$key]->content_clean = htmlspecialchars_decode($segment->content_clean);
         }
 
-        //$source = Segments_SegmentDao::getSourceOrdered($id_job, $where, $order_source, $amount);
         $source = Segments_SegmentDao::getTypeOrderedByJobId($id_job, 'source');
         foreach ($source as $key => $segment){
             $source[$key]->content_raw   = Aligner\Utils\AlignUtils::_mark_xliff_tags($segment->content_raw);
