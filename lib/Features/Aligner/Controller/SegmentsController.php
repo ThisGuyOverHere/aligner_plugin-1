1505119a1f0510077ec7db04795799e680c2b484
Vincenzo Ruffa <vincenzoruffa1991@gmail.com>
2018-11-19 13:56:01 +0100

    JobPasswordValidator in SegmentController

--- lib/Features/Aligner/Controller/SegmentsController.php
+++ lib/Features/Aligner/Controller/SegmentsController.php
@@ -8,32 +8,36 @@
 
 namespace Features\Aligner\Controller;
 
-use Exceptions\NotFoundError;
-use Features\Aligner;
+use Features\Aligner\Utils\AlignUtils;
 use Features\Aligner\Model\Segments_SegmentDao;
-use Features\Aligner\Model\Jobs_JobDao;
+use Features\Aligner\Controller\Validators\JobPasswordValidator;
 
 class SegmentsController extends AlignerController {
 
+    protected $job;
+
+    public function afterConstruct() {
+        $jobValidator = ( new JobPasswordValidator( $this ) );
+
+        $jobValidator->onSuccess( function () use ( $jobValidator ) {
+            $this->job     = $jobValidator->getJob();
+        } );
+
+        $this->appendValidator( $jobValidator );
+    }
+
     public function get(){
-
-        $id_job = $this->params['id_job'];
-        $password = $this->params['password'];
-
-        $job = Jobs_JobDao::getByIdAndPassword( $id_job, $password );
-        if(empty($job)){
-            throw new NotFoundError( "Invalid job id/password pair" );
-        }
+        $id_job = $this->job->id;
 
         $target = Segments_SegmentDao::getTypeOrderedByJobId($id_job, 'target');
         foreach ($target as $key => $segment){
-            $target[$key]->content_raw   = Aligner\Utils\AlignUtils::_mark_xliff_tags($segment->content_raw);
+            $target[$key]->content_raw   = AlignUtils::_mark_xliff_tags($segment->content_raw);
             $target[$key]->content_clean = htmlspecialchars_decode($segment->content_clean);
         }
 
         $source = Segments_SegmentDao::getTypeOrderedByJobId($id_job, 'source');
         foreach ($source as $key => $segment){
-            $source[$key]->content_raw   = Aligner\Utils\AlignUtils::_mark_xliff_tags($segment->content_raw);
+            $source[$key]->content_raw   = AlignUtils::_mark_xliff_tags($segment->content_raw);
             $source[$key]->content_clean = htmlspecialchars_decode($segment->content_clean);
         }
         $this->response->json(['target' => $target, 'source' => $source]);
