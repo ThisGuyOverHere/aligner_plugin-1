ed19a9f17d9088d75e3ff8aecb6a92915a0f46c3
Marco Ciotoli <marco@kromin.it>
2018-11-19 16:30:08 +0100

    Removed unused import

--- app/src/Components/Align/Job/Row/Segment/SegmentContent/SegmentContent.component.js
+++ app/src/Components/Align/Job/Row/Segment/SegmentContent/SegmentContent.component.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import PropTypes from "prop-types";
-import ProjectStore from "../../../../../../Stores/Project.store";
-import ProjectConstants from "../../../../../../Constants/Project.constants";
 
 class SegmentContentComponent extends Component {
     static propTypes = {
