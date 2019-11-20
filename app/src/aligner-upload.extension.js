e5243b4015068b6a5043b604dbdca6fc5e3b6cd3
riccio82 <riccio82@gmail.com>
2019-11-20 10:50:15 +0100

    Added trigger event to dispatch ga event

--- app/src/aligner-upload.extension.js
+++ app/src/aligner-upload.extension.js
@@ -1,8 +1,11 @@
 (function() {
     function overrideHeader( Header ) {
         if ( Header ) {
+            Header.prototype.openAligner = function (  ) {
+                $(document).trigger('aligner-click');
+            };
             Header.prototype.getMoreLinks = function (  ) {
-                return <li><a className="bigblue" href="/plugins/aligner/index#" target="_blank">Aligner</a></li>
+                return <li><a className="bigblue" href="/plugins/aligner/index#" target="_blank" onClick={()=>this.openAligner()}>Aligner</a></li>
             }
         }
     }
